30cd100fb4be0a312269fa008d95ec106492926c
ericminio <eric.mignot@gmail.com>
2015-01-18 10:52:59 -0500

    hide solution of Diamond challenge

--- challenges/diamonds.spec.js
+++ challenges/diamonds.spec.js
@@ -1,28 +1,4 @@
-var spaces = function(count) {
-    blank = '';
-    for (var i=0; i<count; i++) { blank += ' '; }
-    return blank;
-};
-
-var sculpt = function(code) {
-    return String.fromCharCode(code);
-};
-
-var decorate = function(index) {
-    if (index ===0) { return sculpt(65); }
-    return sculpt(65+(index)) + spaces(1+2*(index-1)) + sculpt(65+(index))
-};
-
 var diamond = function(size) {
-    var lines = [];        
-    for (var i=0; i<size; i++) {
-        lines.push(spaces(size-(i+1)) + decorate(i));    
-    }
-    
-    var above = lines;
-    var horizon = lines.pop();
-    var below = lines.join('.').split('.').reverse();
-    return above.concat(horizon, below);
 };
 
 var he = it;
@@ -41,25 +17,5 @@ describe('Diamond creator', function() {
                 '   A'
             ]);
         });        
-    });
-    
-    describe('steps:', function() {
-        
-        he('can create space', function() {
-            expect(spaces(5)).toEqual('     ');
-        });   
-        
-        he('can sculpt precicely', function() {
-            expect(sculpt(65)).toEqual('A');
-            expect(sculpt(66)).toEqual('B');
-        });   
-        
-        he('can decorate', function() {
-            expect(decorate(1)).toEqual('B B');
-        });
-        
-        he('can contain enthousiasm', function() {
-            expect(decorate(0)).toEqual('A');
-        });
-    });
+    });    
 });
