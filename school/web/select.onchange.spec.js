7dd67222c350fdc1025ed554342922c3f576c625
ericminio <eric.mignot@gmail.com>
2015-10-13 13:22:54 -0400

    document select.onchange event

---
+++ school/web/select.onchange.spec.js
@@ -0,0 +1,74 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+require('chai').use(require('sinon-chai'));
+var jsdom = require("jsdom");
+
+describe('Triggering script via select onchange', function() {
+
+    beforeEach(function() {
+        server = require('http').createServer(function(request, response) {
+            var index = '' +
+            '<html>' +
+            '   <head>' +
+            '       <title>initial title</title>' +
+            '       <script src="/jquery.js"></script>' +
+            '       <script src="/script.js"></script>' +
+            '   </head>' +
+            '   <body>' +
+            '       <select id="countries" onchange="displayId(this.options[this.selectedIndex].id);">' +
+            '           <option id="china">China</option>' +
+            '           <option id="canada">Canada</option>' +
+            '       </select>' +
+            '       <label id="continent"></label>' +
+            '   </body>' +
+            '</html>';
+            var script =    'function displayId(id) { ' +
+                            '   document.querySelector("#continent").innerHTML = id;' +
+                            '}';
+
+            if (request.url == '/') {
+                response.writeHead(200, { 'content-type':'text/html' });
+                response.end(index);
+            }
+            if (request.url == '/script.js') {
+                response.writeHead(200, { 'content-type':'application/javascript' });
+                response.end(script);
+                return;
+            }
+            if (request.url == '/jquery.js') {
+                response.writeHead(200, { 'content-type':'application/javascript' });
+                var fs = require('fs');
+                var jqueryLib = require('path').join(__dirname, '/lib/jquery-2.1.3.min.js');
+                var content = fs.readFileSync(jqueryLib).toString();
+                response.end(content);
+                return;
+            }
+        }).listen(5000);
+    });
+
+    afterEach(function() {
+        server.close();
+    });
+
+    it('works', function(exit) {
+        jsdom.env({
+          url: "http://localhost:5000/",
+          features: {
+              FetchExternalResources: ["script"],
+              ProcessExternalResources: ["script"]
+          },
+          done: function (errors, window) {
+              var option = window.document.querySelector('option#canada');
+              option.setAttribute('selected', 'selected');
+
+              var select = window.document.querySelector('#countries');
+              var change = window.document.createEvent('Event');
+              change.initEvent('change', true, true);
+              select.dispatchEvent(change);
+
+              expect(window.document.querySelector('#continent').innerHTML).to.equal('canada');
+              exit();
+          }
+        });
+    });
+});
