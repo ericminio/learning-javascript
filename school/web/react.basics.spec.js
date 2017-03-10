fc54cbb4cb4129a56ca5154d5e50498da0d2964a
Eric Mignot <eric.mignot@gmail.com>
2017-03-10 14:32:46 -0500

    react hello world

---
+++ school/web/react.basics.spec.js
@@ -0,0 +1,66 @@
+const Browser = require('zombie');
+var browser = new Browser();
+
+describe.only('React HelloWorld', function() {
+
+    var app;
+    var server;
+    var port = 5000;
+
+    beforeEach(function(done) {
+
+        var sendPage = function(response) {
+            response.setHeader('Content-Type', 'text/html');
+            var page = '<html>' +
+            '<head>' +
+                '<script src="react-15.3.1.min.js"></script>' +
+                '<script src="react-dom-15.3.1.min.js"></script>' +
+            '</head>' +
+            '<body>'+
+                '<div id="root"></div>' +
+                '<script>'+
+                    'ReactDOM.render('+
+                    '   React.createElement("h1", { id: "greetings" }, "Hello World!"),'+
+                    '   document.getElementById("root")'+
+                    ');' +
+                '</script>'+
+            '</body>'+
+            '</html>';
+            response.write(page);
+        };
+
+        var send = function(file, response) {
+            var angular = require('path').join(__dirname, '/lib/' + file);
+            var content = require('fs').readFileSync(angular).toString();
+            response.setHeader('Content-Type', 'text/plain');
+            response.write(content);
+        };
+
+        var handler = function(request, response) {
+            var url = require('url');
+            var parsed = url.parse(request.url, true);
+            if (parsed.pathname.endsWith('.js')) {
+                send(parsed.pathname, response);
+            }
+            else {
+                sendPage(response);
+            }
+            response.end();
+        };
+
+        app = require('http').createServer(handler);
+        app.listen(port, done);
+    });
+
+    afterEach(function() {
+        app.close();
+    });
+
+    it('works', function(done) {
+        browser.visit('http://localhost:' + port)
+            .then(function() {
+                browser.assert.text('#greetings', 'Hello World!');
+            })
+            .then(done, done);
+    });
+});
