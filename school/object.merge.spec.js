a6844283f92c52ec29545dd1fff83d4f7daec002
Eric Mignot <eric.mignot@gmail.com>
2017-12-28 12:41:50 -0800

    first doc for Object.merge

---
+++ school/object.merge.spec.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+
+describe.only('Object assign', function() {
+
+	it('can be used to merge two objects', function() {
+		var first = { one: 'one', two: 'two' };
+		var second = { three: 'three' };
+		Object.assign(first, second);
+
+		expect(first).to.deep.equal({ one: 'one', two: 'two', three: 'three' });
+	});
+
+    it('keeps source untouched', function() {
+		var first = { one: 'one', two: 'two' };
+		var second = { three: 'three' };
+		Object.assign(first, second);
+
+		expect(second).to.deep.equal({ three: 'three' });
+	});
+
+    it('overrides existing field', function() {
+		var first = { one: 'one', two: 'two' };
+		var second = { two: '***' };
+		Object.assign(first, second);
+
+		expect(first).to.deep.equal({ one: 'one', two: '***' });
+	});
+});
