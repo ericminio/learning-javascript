7479d0623bf10d40fd8eebd6057043103c83048f
ericminio <eric.mignot@gmail.com>
2017-12-31 21:47:47 -0800

    More doc of React in browser with pure js

--- school/web/react.basics.spec.js
+++ school/web/react.basics.spec.js
@@ -7,34 +7,77 @@ describe('React HelloWorld', function() {
     var app;
     var port = 5000;
 
-    beforeEach(function(done) {
-        app = server({ index:'' +
-        '<html>' +
-            '<head>' +
-                '<script src="react-15.3.1.min.js"></script>' +
-                '<script src="react-dom-15.3.1.min.js"></script>' +
-            '</head>' +
-            '<body>'+
-                '<div id="root"></div>' +
-                '<script>'+
-                    'ReactDOM.render('+
-                    '   React.createElement("h1", { id: "greetings" }, "Hello World!"),'+
-                    '   document.getElementById("root")'+
-                    ');' +
-                '</script>'+
-            '</body>'+
-        '</html>' }).listen(port, done);
+    describe('with html element', function() {
+        beforeEach(function(done) {
+            app = server({ index:'' +
+            '<html>' +
+                '<head>' +
+                    '<script src="react-15.6.2.min.js"></script>' +
+                    '<script src="react-dom-15.6.2.min.js"></script>' +
+                '</head>' +
+                '<body>'+
+                    '<div id="root"></div>' +
+                    '<script>'+
+                        'ReactDOM.render('+
+                        '   React.createElement("h1", { id: "greetings" }, "Hello World!"),'+
+                        '   document.getElementById("root")'+
+                        ');' +
+                    '</script>'+
+                '</body>'+
+            '</html>' }).listen(port, done);
+        });
+
+        afterEach(function() {
+            app.close();
+        });
+
+        it('works', function(done) {
+            browser.visit('http://localhost:' + port)
+                .then(function() {
+                    browser.assert.text('#greetings', 'Hello World!');
+                })
+                .then(done, done);
+        });
     });
 
-    afterEach(function() {
-        app.close();
+    describe('with class', function() {
+        beforeEach(function(done) {
+            app = server({ index:'' +
+            '<html>' +
+                '<head>' +
+                    '<script src="react-15.6.2.min.js"></script>' +
+                    '<script src="react-dom-15.6.2.min.js"></script>' +
+                    '<script src="create-react-class-15.6.2.min.js"></script>' +
+                '</head>' +
+                '<body>'+
+                    '<div id="root"></div>' +
+                    '<script>'+
+                        'var Greeting = createReactClass({' +
+                        '    render: function() {' +
+                        '       return React.createElement("h1", { id: "greetings" }, "Hello World!")'+
+                        '    }'+
+                        '});' +
+                        'ReactDOM.render('+
+                        '   React.createElement(Greeting, {}, null),'+
+                        '   document.getElementById("root")'+
+                        ');' +
+                    '</script>'+
+                '</body>'+
+            '</html>' }).listen(port, done);
+        });
+
+        afterEach(function() {
+            app.close();
+        });
+
+        it('works', function(done) {
+            browser.visit('http://localhost:' + port)
+                .then(function() {
+                    browser.assert.text('#greetings', 'Hello World!');
+                })
+                .then(done, done);
+        });
     });
 
-    it('works', function(done) {
-        browser.visit('http://localhost:' + port)
-            .then(function() {
-                browser.assert.text('#greetings', 'Hello World!');
-            })
-            .then(done, done);
-    });
+
 });
