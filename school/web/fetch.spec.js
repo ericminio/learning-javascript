54fe062203209e86228511f3b06458774a9c15a1
Eric Mignot <eric.mignot@gmail.com>
2019-06-06 15:14:42 -0700

    document POST feature of Fetch

--- school/web/fetch.spec.js
+++ school/web/fetch.spec.js
@@ -11,20 +11,46 @@ describe('Fetch', function() {
         <html>
             <head>
                 <title>initial title</title>
-                <script src="fetch-data.js"></script>
+                <script src="service.js"></script>
             </head>
             <body>
                 <label id="data">waiting...</label>
-                <button id="fetchData" onclick="fetchData();">fetch data</button>
+                <button id="getData" onclick="getData();">fetch data</button>
+
+                <input id="field" />
+                <button id="postData" onclick="postData();">fetch data</button>
             </body>
         </html>
     `;
-    var fetchDataScript = function() { return `
-        function fetchData() {
+    var serviceScript = function() { return `
+        function getData() {
             var message = 'http://localhost:` + thirdParty.port + `/any'
             var please = {
                 method: 'GET',
                 mode: 'cors'
+            };
+            fetch(message, please)
+                .then((response)=> {
+                    if (response.ok) {
+                        return response.text()
+                    }
+                    else {
+                        return 'error'
+                    }
+                })
+                .then((text)=> {
+                    document.getElementById('data').innerHTML = text;
+                })
+                .catch((error)=> {
+                    document.getElementById('data').innerHTML = error.message;
+                })
+        }
+        function postData() {
+            var message = 'http://localhost:` + thirdParty.port + `/any'
+            var please = {
+                method: 'POST',
+                mode: 'cors',
+                body: document.getElementById('field').value
             };
             fetch(message, please)
                 .then((response)=> {
@@ -47,13 +73,25 @@ describe('Fetch', function() {
     beforeEach(function(done) {
         thirdParty = new LocalServer(function(request, response) {
             response.setHeader('Access-Control-Allow-Origin', '*');
-            response.write('data coming from third party');
-            response.end();
+            if (request.method == 'POST') {
+                var body = ''
+                request.on('data', (chunk)=>{
+                    body += chunk
+                })
+                request.on('end', ()=>{
+                    response.write('pong: ' + body);
+                    response.end();
+                })
+            }
+            else {
+                response.write('pong');
+                response.end();
+            }
         });
         thirdParty.start(function() {
             server = new LocalServer({
                 '/': index,
-                '/fetch-data.js': fetchDataScript()
+                '/service.js': serviceScript()
             });
             server.start(done);
         });
@@ -75,13 +113,38 @@ describe('Fetch', function() {
                 let document = dom.window.document;
                 addFetchFeature(document)
 
-                let button = document.getElementById('fetchData');
+                let button = document.getElementById('getData');
                 button.click();
 
                 setTimeout(()=>{
                     var label = document.getElementById('data')
 
-                    expect(label.innerHTML).to.equal('data coming from third party')
+                    expect(label.innerHTML).to.equal('pong')
+                    done()
+                },300)
+            }, 500);
+        });
+    });
+
+    it('can POST data', function(done) {
+        JSDOM.fromURL('http://localhost:'+server.port, {
+            runScripts: 'dangerously',
+            resources: 'usable'
+        })
+        .then((dom)=>{
+            setTimeout(() => {
+                let document = dom.window.document;
+                addFetchFeature(document)
+
+                let input = document.getElementById('field')
+                input.value = 'hello'
+                let button = document.getElementById('postData');
+                button.click();
+
+                setTimeout(()=>{
+                    var label = document.getElementById('data')
+
+                    expect(label.innerHTML).to.equal('pong: hello')
                     done()
                 },300)
             }, 500);
