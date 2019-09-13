54846028ef932013ba088e09ed2899c7d5eac8f0
Eric Mignot <eric.mignot@gmail.com>
2019-09-13 13:33:56 -0700

    also running selenium test

--- school/tests/selenium.spec.js
+++ school/tests/selenium.spec.js
@@ -2,57 +2,42 @@ var chai = require('chai');
 chai.use(require('chai-string'));
 var expect = chai.expect;
 
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By;
-var Capabilities = require('selenium-webdriver/lib/capabilities').Capabilities;
-var capabilities = Capabilities.firefox();
-capabilities.set('marionette', false);
+const { Builder, By } = require('selenium-webdriver')
+const firefox = ()=> new Builder().forBrowser('firefox').build()
 
-xdescribe('Selenium', function() {
+describe('Selenium', function() {
 
     var server;
     var driver;
 
     beforeEach(function(done) {
+        driver = firefox();
         server = require('http').createServer(function(request, response) {
-            var index = '' +
-            '<html>' +
-            '   <head><title>hello</title></head>' +
-            '   <body>' +
-            '       <div id="greetings" style="display:inline-block">welcome</div>' +
-            '   </body>' +
-            '</html>';
-            response.writeHead(200, { 'content-type':'text/html' });
-            response.end(index);
-        }).listen(5000, done);
-    });
+            var page = `
+                <html>
+                    <head>
+                        <title>hello</title>
+                    </head>
+                    <body>
+                        <div id="greetings" style="display:inline-block">welcome</div>
+                    </body>
+                </html>
+            `
+            response.writeHead(200, { 'content-type':'text/html' })
+            response.end(page)
+        }).listen(5000, done)
+    })
 
-    afterEach(function() {
+    afterEach(async ()=> {
         server.close();
-        driver.quit();
-    });
+        await driver.quit();
+    })
 
-    it('can be used to inspect the computed width of an element', function(done) {
-        this.timeout(5000);
-        driver = new webdriver.Builder().withCapabilities(capabilities).build();
-        driver.get('http://localhost:5000/').then(
-            function() {
-                var element = driver.findElement(By.id('greetings'));
-                element.getCssValue('width').then(
-                    function(value) {
-                        expect(value).to.endWith('px');
-                        done();
-                    },
-                    function(error) {
-                        expect(JSON.stringify(error)).to.equal(undefined);
-                        done();
-                    }
-                );
-            },
-            function(error) {
-                expect(JSON.stringify(error)).to.equal(undefined);
-                done();
-            }
-        );
-    });
-});
+    it('can be used to inspect the computed width of an element', async ()=> {
+        await driver.get('http://localhost:5000/')
+        let element = await driver.findElement(By.id('greetings'))
+        let value = await element.getCssValue('width')
+
+        expect(value).to.endWith('px')
+    })
+})
