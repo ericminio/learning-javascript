79253fd942c312c0bad4146435aedae8a3f887bc
ericminio <eric.mignot@gmail.com>
2021-08-11 08:56:35 -0700

    stringify

--- school/linked-list.spec.js
+++ school/linked-list.spec.js
@@ -1,42 +1,47 @@
 var expect = require('chai').expect;
 
-describe('Linked list inversion', function() {
+describe.only('Linked list inversion', function() {
 
     it('is easy with one element', ()=>{
         let head = { value:42, next:undefined };
-        expect(inverted(head)).to.deep.equal({ value:42, next:undefined });
+        expect(invert(head)).to.deep.equal({ value:42, next:undefined });
     });
+    let list = (input)=> {
+        let nodes = [];
+        let values = input.split('-->');
+        for (let i=0; i<values.length; i++) {
+            let node = { value: parseInt(values[i]), next:undefined} ;
+            nodes.push(node);
+            if (i > 0) {
+                nodes[i-1].next = node;
+            }
+        }
+        return nodes[0];
+    };
+    let stringify = (head)=> {
+        let current = head;
+        let safe = 100;
+        let output = '' + current.value;
+        while (current.next !== undefined && safe > 0) {
+            current = current.next;
+            safe --;
+            output += ' --> ' + current.value;
+        }
+        return output;
+    }
     it('also works with two elements', ()=>{
-        let head = { 
-            value:1, 
-            next:{ 
-                value:2, 
-                next:undefined } };
-        expect(inverted(head)).to.deep.equal({ 
-            value:2, 
-            next:{ 
-                value:1, 
-                next:undefined }});
+        let head = list('1 --> 2');
+        let inverted = invert(head);
+        expect(stringify(inverted)).to.equal('2 --> 1');
     });
     it('also works with thress elements', ()=>{
-        let head = { 
-            value:1, 
-            next:{ 
-                value:2, 
-                next:{ 
-                    value:3, 
-                    next:undefined } } };
-        expect(inverted(head)).to.deep.equal({ 
-            value:3, 
-            next:{ 
-                value:2, 
-                next:{ 
-                    value:1, 
-                    next:undefined } }});
+        let head = list('1 --> 2 --> 3');
+        let inverted = invert(head);
+        expect(stringify(inverted)).to.equal('3 --> 2 --> 1');
     });
 });
 
-var inverted = (head)=> {
+var invert = (head)=> {
     let previous = undefined, 
         current = head,
         next = current.next;
