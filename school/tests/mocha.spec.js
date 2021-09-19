246a86dc88cdad37994dba577285e30a3384fb70
ericminio <eric.mignot@gmail.com>
2021-09-18 19:06:49 -0700

    about chai-as-promised

--- school/tests/mocha.spec.js
+++ school/tests/mocha.spec.js
@@ -1,4 +1,4 @@
-var expect = require('chai').expect;
+var { expect } = require('chai');
 
 describe('Mocha', function () {
 
@@ -33,6 +33,51 @@ describe('Mocha', function () {
             sut.api(collaborator);
 
             expect(collaborator.doThat).to.have.been.calledWith('please');
+        });
+    });
+
+    describe('with promises', ()=> {
+
+        it('can expect resolution', ()=> {
+            return Promise.resolve(42)
+                .then((value)=> { expect(value).to.equal(42); } );
+        });
+        it('can expect rejection', ()=> {
+            return Promise.reject(66)
+                .catch((value)=> { expect(value).to.equal(66); } );
+        });
+    });
+
+    describe('with chai-as-promised', ()=> {
+
+        let chai = require('chai');
+        var chaiAsPromised = require("chai-as-promised");
+        chai.use(chaiAsPromised);
+
+        it('can expect resolution', ()=> {
+            return expect(Promise.resolve(42))
+                .to.eventually.equal(42);
+        });
+        it('can expect resolved object', ()=> {
+            return expect(Promise.resolve({ value:42 }))
+                .to.eventually.deep.equal({ value:42 });
+        });
+        it('can expect resolved object field', ()=> {
+            return expect(Promise.resolve({ value:42 }))
+                .to.eventually.have.property('value', 42);
+        });
+        
+        it('can expect rejection', ()=> {
+            return expect(Promise.reject(66))
+                .to.be.rejected.then(error => { expect(error).to.equal(66); });
+        });
+        it('can expect rejected object field', ()=> {
+            return expect(Promise.reject({ value:66 }))
+                .to.eventually.be.rejected.and.have.property('value', 66);
+        });
+        it('can expect rejected object', ()=> {
+            return expect(Promise.reject({ value:66 }))
+                .to.be.rejected.then(error => { expect(error).to.deep.equal({ value:66 }); });
         });
     });
 });
