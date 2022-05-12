ab19dbf7a3366011d0d1b3838a01f75a51145f77
ericminio <emignot@change.org>
2022-05-12 13:24:59 -0700

    prime factors

---
+++ challenges/prime.spec.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+
+describe('prime factors', () => {
+
+    it('starts simple', () => {
+        expect(primeFactorsOf(2)).to.deep.equal([2]);
+    });
+    it('can compute power of 2', () => {
+        expect(primeFactorsOf(16)).to.deep.equal([2, 2, 2, 2]);
+    });
+    it('can compute power of 3', () => {
+        expect(primeFactorsOf(9)).to.deep.equal([3, 3]);
+    });
+    it('can compute any number', () => {
+        expect(primeFactorsOf(1492)).to.deep.equal([2, 2, 373]);
+    });
+});
+
+const primeFactorsOf = (number) => {
+    let factors = [];
+    let candidate = 2;
+
+    while (number > 1) {
+        while (number % candidate == 0) {
+            factors.push(candidate);
+            number /= candidate;
+        }
+        candidate++;
+    }
+
+    return factors;
+}
