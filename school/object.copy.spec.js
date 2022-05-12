80be7030f501c9c34a80c33e7bff8f05f8ba58d1
ericminio <emignot@change.org>
2022-05-12 15:32:44 -0700

    about object copy

---
+++ school/object.copy.spec.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;
+
+describe('Object copy', function () {
+
+	it('has its own syntax', function () {
+		var first = { one: 'one', two: 'two' };
+		var second = { ...first };
+		first.one = '1';
+
+		expect(second.one).to.equal('one');
+	});
+
+	it('has a syntax to include field overwrite', function () {
+		var first = { one: 'one', two: 'two' };
+		var second = { ...first, two: '2' };
+
+		expect(second).to.deep.equal({ one: 'one', two: '2' });
+	});
+});
--- school/object.merge.spec.js
+++ school/object.merge.spec.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect;
 
-describe('Object assign', function() {
+describe('Object assign', function () {
 
-	it('can be used to merge two objects', function() {
+	it('can be used to merge two objects', function () {
 		var first = { one: 'one', two: 'two' };
 		var second = { three: 'three' };
 		Object.assign(first, second);
@@ -10,7 +10,7 @@ describe('Object assign', function() {
 		expect(first).to.deep.equal({ one: 'one', two: 'two', three: 'three' });
 	});
 
-    it('keeps source untouched', function() {
+	it('keeps source untouched', function () {
 		var first = { one: 'one', two: 'two' };
 		var second = { three: 'three' };
 		Object.assign(first, second);
@@ -18,7 +18,7 @@ describe('Object assign', function() {
 		expect(second).to.deep.equal({ three: 'three' });
 	});
 
-    it('overrides existing field', function() {
+	it('overrides existing field', function () {
 		var first = { one: 'one', two: 'two' };
 		var second = { two: '***' };
 		Object.assign(first, second);
