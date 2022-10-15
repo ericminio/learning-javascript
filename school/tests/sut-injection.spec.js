dd57c8f3388722b421e50314eb47584b08ed82ca
ericminio <eric.mignot@gmail.com>
2022-10-15 08:45:31 -0700

    about sut injection

---
+++ school/tests/sut-injection.spec.js
@@ -0,0 +1,20 @@
+var { expect } = require('chai');
+
+const verify = (sut) => {
+
+    describe('expectation', () => {
+        
+        it('is met', () => {
+            expect(sut()).to.equal(42);
+        })
+    });
+};
+
+const firstSut = () => 42;
+const secondSut = () => 84 / 2;
+
+describe('SUT injection', function () {
+
+    verify(firstSut);
+    verify(secondSut);
+});
