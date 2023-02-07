3e4c6242da50a94e3c57f4773c90a6d72e46d1a4
ericminio <eric.mignot@gmail.com>
2023-02-06 19:29:50 -0800

    ttt

--- school/web/websocket.spec.js
+++ school/web/websocket.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const { isFrameFinal, isText, decodeLength, extractMask, extractData, decode, encode } = require('./websocket-frames.js');
 
-describe.only('decoding of small text in one frame', () => {
+describe('decoding of small text in one frame', () => {
 
     const abcdef = Buffer.from([0x81, 0x86, 0xb0, 0x4c, 0x40, 0xcd, 0xd1, 0x2e, 0x23, 0xa9, 0xd5, 0x2a]);
 
@@ -39,7 +39,7 @@ describe.only('decoding of small text in one frame', () => {
 
 });
 
-describe.only('encoding of small text in one frame', () => {
+describe('encoding of small text in one frame', () => {
 
     it('does not require a mask', () => {
         expect(encode('a')).to.deep.equal(Buffer.from([0x81, 1, 97]));
