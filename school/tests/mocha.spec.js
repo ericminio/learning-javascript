4fadd0cea4b77c18725cbc97f9d1cc155340be87
ericminio <eric.mignot@gmail.com>
2023-02-06 19:35:43 -0800

    ttt

--- school/tests/mocha.spec.js
+++ school/tests/mocha.spec.js
@@ -36,29 +36,6 @@ describe.only('Mocha', function () {
         });
     });
 
-    describe('Spying with sinon', function () {
-
-        var sinon = require('sinon');
-
-        it('offers spying on the method of an object', function () {
-            var sut = { api: function (dependency) { dependency.doThat('please'); } };
-            var collaborator = { doThat: function () { } };
-            var spy = sinon.spy(collaborator, 'doThat');
-            sut.api(collaborator);
-
-            expect(spy.calledWith('please')).to.equal(true);
-        });
-
-        it('gives better assertion errors with sinon-chai', function () {
-            require('chai').use(require('sinon-chai'));
-            var sut = { api: function (dependency) { dependency.doThat('please'); } };
-            var collaborator = { doThat: sinon.spy() };
-            sut.api(collaborator);
-
-            expect(collaborator.doThat).to.have.been.calledWith('please');
-        });
-    });
-
     describe('with promises', () => {
 
         it('can expect resolution', () => {
