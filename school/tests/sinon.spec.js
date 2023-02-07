b9cde522436ce567c5fc06162f6703c12ab1f041
ericminio <eric.mignot@gmail.com>
2023-02-06 19:35:05 -0800

    ttt

--- school/tests/sinon.spec.js
+++ school/tests/sinon.spec.js
@@ -1,46 +1,11 @@
 var { expect } = require('chai');
+var sinon = require('sinon');
 
 describe.only('Sinon', function () {
 
-    it('can assert', function () {
-        expect(1 + 1).to.equal(2);
-    });
-    it('can require node core modules', function () {
-        expect(require('http')).not.to.equal(undefined);
-        expect(require('fs')).not.to.equal(undefined);
-    });
-    it('can require sinon', function () {
-        expect(require('sinon')).not.to.equal(undefined);
-    });
-    it('exposes test name in this.ctx.test.title with arrow function', () => {
-        expect(this.ctx.test.title).to.equal('exposes test name in this.ctx.test.title with arrow function');
-    });
-    it('exposes test name in this.test.title with regular function', function () {
-        expect(this.test.title).to.equal('exposes test name in this.test.title with regular function');
-    });
-    describe(`
-        multiline
-        context
-        with regular function callback
-        `, function () {
-
-        let title;
-        beforeEach(() => {
-            title = this.title
-                .split('\n')
-                .map(line => line.trim())
-                .filter(line => line.length > 0)
-                .join(' ');
-        });
-
-        it('is exposed in this.title', () => {
-            expect(title).to.equal('multiline context with regular function callback');
-        });
-    });
-
     describe('Spying with sinon', function () {
 
-        var sinon = require('sinon');
+
 
         it('offers spying on the method of an object', function () {
             var sut = { api: function (dependency) { dependency.doThat('please'); } };
