7423d44c0af672735ee47a3b6f25e85580c227a2
ericminio <eric.mignot@gmail.com>
2023-02-06 19:51:06 -0800

    ttt

--- school/arrays.spec.js
+++ school/arrays.spec.js
@@ -47,7 +47,7 @@ describe("Arrays", function () {
 		});
 	});
 
-	describe("removing elements", function () {
+	describe.only("removing elements", function () {
 
 		it("'pop' removes the last element", function () {
 			array.push(one);
@@ -224,7 +224,7 @@ describe("Arrays", function () {
 			it('uses 0 to indicate equality', function () {
 				let set = [3, 1, 2];
 				set.sort((a, b) => 0);
-	
+
 				expect(set).to.deep.equal([3, 1, 2])
 			});
 
@@ -234,14 +234,14 @@ describe("Arrays", function () {
 					if (a < b) return 1;
 					if (a > b) return -1;
 				});
-	
+
 				expect(set).to.deep.equal([3, 2, 1])
 			});
 
 			it('also works with positive/negative', function () {
 				let set = [3, 1, 2];
 				set.sort((a, b) => a - b);
-	
+
 				expect(set).to.deep.equal([1, 2, 3])
 			});
 		});
