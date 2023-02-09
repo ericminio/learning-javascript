e518db65f08985615a439dbd80330935389dcad5
ericminio <emignot@change.org>
2023-02-09 14:40:40 -0800

    ttt

--- school/tests/sinon.spec.js
+++ school/tests/sinon.spec.js
@@ -44,7 +44,7 @@ describe.only('Sinon', function () {
                 expect('should fail').to.equal('but no');
             }
             catch (error) {
-                console.log(error.error)
+                console.log(error)
                 // expect(error).to.equal(`Cannot read property 'data' of undefined`)
             }
         });
