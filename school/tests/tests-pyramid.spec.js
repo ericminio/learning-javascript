18ab51248212cba0e741673324181e797071c992
ericminio <eric.mignot@gmail.com>
2023-02-09 20:42:17 -0800

    ttt

--- school/tests/tests-pyramid.spec.js
+++ school/tests/tests-pyramid.spec.js
@@ -12,13 +12,13 @@ describe.only('Tests', () => {
         fetch = sinon.stub();
     });
 
-    it('can describe behaviors observable from outside', () => {
+    it('can be external and describe behaviors observable from outside', () => {
         fetch.returns(42);
 
         expect(a.doThat(b, fetch)).to.equal(42);
     });
 
-    it('can describe internal behaviors', () => {
+    it('can be internal and describe internal collaboration', () => {
         b.doThis(fetch);
 
         expect(fetch).to.have.been.calledWith('key');
