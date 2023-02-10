f021a01248f49782c3ce640d1bbd06a17ee4cc28
ericminio <eric.mignot@gmail.com>
2023-02-09 20:49:51 -0800

    ttt

--- school/tests/tests-pyramid.spec.js
+++ school/tests/tests-pyramid.spec.js
@@ -2,9 +2,6 @@ var { expect } = require('chai');
 var sinon = require('sinon');
 require('chai').use(require('sinon-chai'));
 
-const service = { doThat: (b, fetch) => b.doThis(fetch) };
-const b = { doThis: fetch => fetch('key') };
-
 describe.only('Tests', () => {
 
     let fetch;
@@ -15,12 +12,26 @@ describe.only('Tests', () => {
     it('can be external and describe behaviors observable from outside', () => {
         fetch.returns(42);
 
-        expect(service.doThat(b, fetch)).to.equal(42);
+        expect(new Service().doThat(new Adapter(fetch))).to.equal(42);
     });
 
     it('can be internal and describe internal details', () => {
-        b.doThis(fetch);
+        new Adapter(fetch).doThis();
 
         expect(fetch).to.have.been.calledWith('key');
     });
 });
+
+class Service {
+    doThat(port) {
+        return port.doThis();
+    }
+};
+class Adapter {
+    constructor(fetch) {
+        this.fetch = fetch;
+    }
+    doThis() {
+        return this.fetch('key');
+    }
+};
