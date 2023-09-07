8ebe14cc9c8bb0a1b6658b0920759cffda251b21
Eric Mignot <eric.mignot@gmail.com>
2023-09-07 10:32:05 -0700

    about native node mocking

---
+++ school/node/mocking.test.js
@@ -0,0 +1,16 @@
+const { describe, it, beforeEach, afterEach } = require('node:test');
+const { strict: assert } = require('node:assert');
+
+const sut = {
+    add: (a, b) => a + b,
+};
+
+describe('mocking', () => {
+    it('allows call assertion', (context) => {
+        context.mock.method(sut, 'add');
+        sut.add(1, 2);
+
+        assert.strictEqual(sut.add.mock.calls.length, 1);
+        assert.deepStrictEqual(sut.add.mock.calls[0].arguments, [1, 2]);
+    });
+});
