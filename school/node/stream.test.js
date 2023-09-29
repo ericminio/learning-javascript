af1370adcf3687629d97e5f0614f1c9782e9b549
Eric Mignot <eric.mignot@gmail.com>
2023-09-29 15:53:59 -0700

    read file one char at a time

--- school/node/stream.test.js
+++ school/node/stream.test.js
@@ -1,6 +1,7 @@
 const { describe, it } = require('node:test');
 const { strict: assert } = require('node:assert');
 const { Readable } = require('node:stream');
+const { open } = require('node:fs/promises');
 
 describe('Readable Stream', () => {
     it('can be built from array', async () => {
@@ -16,5 +17,43 @@ describe('Readable Stream', () => {
             });
         });
         assert.deepStrictEqual(received, [3, 5, 7]);
+    });
+
+    it('can read a file one char at a time', async () => {
+        const fd = await open('./school/node/data/a-file.txt');
+        const stream = fd.createReadStream({ highWaterMark: 1 });
+        const received = await new Promise((resolve) => {
+            const buffer = [];
+            stream.on('data', (chunk) => {
+                buffer.push(chunk.toString());
+            });
+            stream.on('close', () => {
+                resolve(buffer);
+            });
+        });
+        assert.deepStrictEqual(received, [
+            'f',
+            'i',
+            'r',
+            's',
+            't',
+            ' ',
+            'l',
+            'i',
+            'n',
+            'e',
+            '\n',
+            's',
+            'e',
+            'c',
+            'o',
+            'n',
+            'd',
+            ' ',
+            'l',
+            'i',
+            'n',
+            'e',
+        ]);
     });
 });
