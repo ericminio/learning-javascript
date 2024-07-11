b146025430bd98178f155100d7c03a1075804140
ericminio <eric.mignot@gmail.com>
2024-07-10 21:06:24 -0700

    relative dates

--- school/dates.js
+++ school/dates.js
@@ -10,31 +10,40 @@ Number.prototype.hours = function () {
 };
 Number.prototype.ago = function (ref) {
     const now = ref || new Date();
-    const date = new Date(now.getTime() - this.valueOf());
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    const hours = date.getHours();
-
-    const before = new Date(year, month, day, hours + 1);
+    const before = new Date(now.getTime() - this.valueOf());
     before.nowWas = now;
     return before;
 };
+Number.prototype.midnightAgo = function (ref) {
+    const now = ref || new Date();
+    const midnight = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        0,
+        0,
+        0,
+        0
+    );
+
+    return new Date(midnight.getTime() - (this.valueOf() - 1).days());
+};
+
+const rstamp = (date) => {
+    if (date.getTime() > (1).midnightAgo().getTime()) {
+        return 'today';
+    }
+    if (date.getTime() > (2).midnightAgo().getTime()) {
+        return 'yesterday';
+    }
+    const now = new Date();
+    const diff = now.getTime() - date.getTime();
+    const n = diff / (1).day();
+
+    return `${n} days ago`;
+};
 
 describe('Dates', function () {
-    it('returns top of the clock', () => {
-        const before = (3).days().ago();
-
-        expect(before.toISOString()).to.match(/:00:00.000Z$/);
-    });
-
-    it('rounds up to the next hour', () => {
-        const now = new Date('2021-10-19T06:03:00.000Z');
-        const before = (3).days().ago(now);
-
-        expect(before.toISOString()).to.equal('2021-10-16T07:00:00.000Z');
-    });
-
     it('exposes what now was at time of calculation', () => {
         const yesterday = (1).day().ago();
         const now = yesterday.nowWas;
@@ -46,8 +55,40 @@ describe('Dates', function () {
 
     it('offers hour granularity', () => {
         const now = new Date('2021-10-19T06:03:00.000Z');
-        const before = (3).hours().ago(now);
+        const before = (2).hours().ago(now);
 
-        expect(before.toISOString()).to.equal('2021-10-19T04:00:00.000Z');
+        expect(before.toISOString()).to.equal('2021-10-19T04:03:00.000Z');
+    });
+});
+
+describe('midnights', () => {
+    it('exposes previous midnight', () => {
+        expect(
+            (1).midnightAgo(new Date(2010, 5, 5, 1, 2, 3)).toISOString()
+        ).to.equal('2010-06-05T00:00:00.000Z');
+    });
+
+    it('exposes 2 midnights ago', () => {
+        expect(
+            (2).midnightAgo(new Date(2010, 5, 5, 1, 2, 3)).toISOString()
+        ).to.equal('2010-06-04T00:00:00.000Z');
+    });
+});
+
+describe('relative timestamp', () => {
+    it('can detect today', () => {
+        expect(rstamp((2).hours().ago())).to.equal('today');
+    });
+
+    it('can detect yesterday', () => {
+        expect(rstamp((1).days().ago())).to.equal('yesterday');
+    });
+
+    it('can detect 2 days ago', () => {
+        expect(rstamp((2).days().ago())).to.equal('2 days ago');
+    });
+
+    it('can detect 3 days ago', () => {
+        expect(rstamp((3).days().ago())).to.equal('3 days ago');
     });
 });
