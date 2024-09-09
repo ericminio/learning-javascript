42dda739e7c4e7285ac484297f96d2798dd6fcf1
Eric Mignot <eric.mignot@gmail.com>
2024-09-09 10:46:03 -0700

    durations

--- challenges/timeago/timeago.test.js
+++ challenges/timeago/timeago.test.js
@@ -1,22 +1,46 @@
 const { describe, it } = require('node:test');
 const { strict: assert } = require('node:assert');
 
+describe('time ago', () => {
+    it('can detect today', () => {
+        const time = lastMidnight().plus({ hours: 1 });
+        const result = timeAgo(time);
+
+        assert.strictEqual(result, 'today');
+    });
+
+    it('can detect yesterday', () => {
+        const time = lastMidnight().minus({ hours: 1 });
+        const result = timeAgo(time);
+
+        assert.strictEqual(result, 'yesterday');
+    });
+
+    it('can detect 3 days ago', () => {
+        const time = lastMidnight().plus({ hours: 1 }).minus({ days: 3 });
+        const result = timeAgo(time);
+
+        assert.strictEqual(result, '3 days ago');
+    });
+});
+
 const lastMidnight = () => {
     const now = new Date();
     return new Date(now.getFullYear(), now.getMonth(), now.getDate());
 };
+const minutes = (value) => value * 60 * 1000;
+const hours = (value) => value * minutes(60);
+const days = (value) => value * hours(24);
+const durations = {
+    hours: hours,
+    days: days,
+};
 
 const durationInMs = (duration) => {
-    return Object.entries(duration).reduce((acc, [unit, value]) => {
-        switch (unit) {
-            case 'hours':
-                return acc + value * 60 * 60 * 1000;
-            case 'days':
-                return acc + value * 24 * 60 * 60 * 1000;
-            default:
-                throw new Error(`Unknown unit: ${unit}`);
-        }
-    }, 0);
+    return Object.entries(duration).reduce(
+        (acc, [unit, value]) => acc + durations[unit](value),
+        0
+    );
 };
 
 Date.prototype.plus = function (duration) {
@@ -41,26 +65,3 @@ const timeAgo = (date) => {
     );
     return `${diff + 1} days ago`;
 };
-
-describe('time ago', () => {
-    it('can detect today', () => {
-        const time = lastMidnight().plus({ hours: 1 });
-        const result = timeAgo(time);
-
-        assert.strictEqual(result, 'today');
-    });
-
-    it('can detect yesterday', () => {
-        const time = lastMidnight().minus({ hours: 1 });
-        const result = timeAgo(time);
-
-        assert.strictEqual(result, 'yesterday');
-    });
-
-    it('can detect 3 days ago', () => {
-        const time = lastMidnight().plus({ hours: 1 }).minus({ days: 3 });
-        const result = timeAgo(time);
-
-        assert.strictEqual(result, '3 days ago');
-    });
-});
